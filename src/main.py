0765068a19b348000636b7003a93334aef3b44f3
Haroon Hussein <hussein.haroon@outlook.com>
2026-08-12 20:24:39 +0100

    Add basic PyBullet simulation environment

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Surgical teleoperation project started successfully.")
+import pybullet as p
+import pybullet_data
+
+physics_client = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+plane_id = p.loadURDF("plane.urdf")
+
+input("Press Enter to close the simulation...")
+
+p.disconnect()
